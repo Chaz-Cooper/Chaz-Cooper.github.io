18501230326b87f4234499787c7f20f4ceff9107
Chaz-Cooper <63518897+Chaz-Cooper@users.noreply.github.com>
2021-10-10 10:51:03 -0700

    Completed wk4
    fixed text-style to work. (forgot to set a style)
    added plain and fancy text
    added Moo button
    made boring text remove -Moo as well.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,6 +1,46 @@
 function bigger()
 {
-    document.getElementById("Bigger");
-    document.getElementById("my-text").style.propertyName = "26pt";
     //alert("Hello, world!");
+    document.getElementById("Bigger");
+    document.getElementById("input-text").style.fontSize = "24pt";
+}
+
+function myAlert()    //alert function
+{
+    alert("Lets get jiggy with it!");
+}
+
+function fancyText()  //fancies input text
+{
+    document.getElementById("input-text").style.fontWeight = "bold";
+    document.getElementById("input-text").style.color = "blue";
+    document.getElementById("input-text").style.textDecoration = "underline";
+}
+
+function boringText() //makes text original
+{
+    document.getElementById("input-text").style.fontWeight = "inherit";
+    document.getElementById("input-text").style.color = "inherit";
+    document.getElementById("input-text").style.textDecoration = "inherit";
+    document.getElementById("input-text").style.textTransform = "inherit";
+
+    var str = document.getElementById("input-text").value;
+    //remove "-moo"
+    var parts = str.split("-Moo.");
+    str = parts.join(".");
+    document.getElementById("input-text").value = str;
+}
+
+function moo()  //adds moo to the end of text
+{
+    document.getElementById("Moo");
+    document.getElementById("input-text").style.textTransform = "uppercase";
+
+    //moo text
+    var str = document.getElementById("input-text").value;
+    //Replace "." with "moo"
+    var parts = str.split(".");
+    str = parts.join("-Moo.");
+
+    document.getElementById("input-text").value = str;
 }
